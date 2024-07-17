61165afd5e4eb6b2936bf944b3c504ffde2c8ec5
elfataa <luthfi.emillulfata@mhs.unsoed.ac.id>
2024-07-17 11:08:54 +0700

    Menambah field pendaftaran di file Registrasi

--- app/src/main/java/com/example/formregistrasi/RegistrasiActivity.java
+++ app/src/main/java/com/example/formregistrasi/RegistrasiActivity.java
@@ -1,14 +1,61 @@
 package com.example.formregistrasi;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import javax.xml.transform.Result;
 
 public class RegistrasiActivity extends AppCompatActivity {
+
+    Button btnPickImage;
+    ImageView fotoKTP;
+
+    ActivityResultLauncher<Intent> resultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrasi);
+
+        btnPickImage = findViewById(R.id.btnPickImage);
+        fotoKTP = findViewById(R.id.fotoKTP);
+        registerResult();
+
+        btnPickImage.setOnClickListener(view -> pickImage());
+        resultLauncher.launch(intent);
+    }
+
+    private void pickImage(){
+        Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES)
+    }
+
+    private void registerResult(){
+        resultLauncher = registerForActivityResult(
+                new ActivityResultContract.StartActivityForResult(),
+                new ActivityResultCallback<Result>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result){
+                        try{
+                            Uri imageUri = result.getData().getData();
+                            fotoKTP.setImageURI(imageUri);
+                        }catch (Exception e){
+                            Toast.makeText(RegistrasiActivity.this, "Tidak ada gambar yang terpilih", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                }
+        )
     }
 }
