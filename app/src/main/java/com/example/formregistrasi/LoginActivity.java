2cdbbe1e7c5d24b7bb628e18b4857ce8a70876a6
elfataa <luthfi.emillulfata@mhs.unsoed.ac.id>
2024-07-18 13:25:58 +0700

    www.com

--- app/src/main/java/com/example/formregistrasi/LoginActivity.java
+++ app/src/main/java/com/example/formregistrasi/LoginActivity.java
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etNama, etNik;
+    private String nama, nik;
+    private String URL = "http://10.10.2.2/pendaftaranPerumdam/login.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
