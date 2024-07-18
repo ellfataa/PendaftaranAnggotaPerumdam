bd26d903ea4cfd8e7c7d3acbd93967f38bf91f23
CzperTama <abhirama.pratama@mhs.unsoed.ac.id>
2024-07-18 11:10:22 +0700

    menambahkan fungsi temukan lokasi

--- app/src/main/java/com/example/formregistrasi/RegistrasiActivity.java
+++ app/src/main/java/com/example/formregistrasi/RegistrasiActivity.java
@@ -1,18 +1,79 @@
 package com.example.formregistrasi;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class RegistrasiActivity extends AppCompatActivity {
+    private TextView Latitude, Longitude;
+    private Button btnTemukan;
+    private FusedLocationProviderClient LocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrasi);
+        Latitude = findViewById(R.id.etLatitude);
+        Longitude = findViewById(R.id.etLongtitude);
+        btnTemukan = findViewById(R.id.btn_temukan);
 
+        LocationProviderClient = LocationServices.getFusedLocationProviderClient(RegistrasiActivity.this);
+        btnTemukan.setOnClickListener(v -> {
+            getLocation();
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 10) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(getApplicationContext(), "izin Lokasi anda tidak diaktifkan", Toast.LENGTH_SHORT).show();
+            } else {
+                getLocation();
+            }
+        }
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[] {
+                    android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION
+            },10);
+        } else {
+            LocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location!=null) {
+                        Latitude.setText(String.valueOf(location.getLatitude()));
+                        Longitude.setText(String.valueOf(location.getLongitude()));
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Lokasi anda tidak aktif!",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 
     public void btnKembali(View view) {
